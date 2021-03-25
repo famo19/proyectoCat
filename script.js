21c96c40bb978b62364023143367b32ef2b58750
Francisco Alejandro Murillo Oviedo <murillo.francisco2001@gmail.com>
2021-03-24 22:09:30 -0600

    Categories and Resumes part v1.0

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function registerNewUser() {
+/*INICIO DE AGREGAR CATEGORÍAS*/
+function registerNewCat() {
     var reg_cat = document.getElementById("cat").value;
 
     //alert(reg_user);
@@ -9,12 +10,15 @@ function registerNewUser() {
     }
 
     var current_reg = {
+        //agregar registro de usuario
         cat: reg_cat
     };
 
     catArray.push(current_reg);
 
     localStorage.setItem("CatAdminArray", JSON.stringify(catArray));
+
+    document.getElementById("cat").value = ""
 
     loadCatDataFromAdmin()
 
@@ -32,10 +36,79 @@ function loadCatDataFromAdmin() {
     var row
 
     for (var cat of catArray) {
-        row = catTable.insertRow(1)
+        row = catTable.insertRow(1);
 
+        //Insertar columna de usuario
         row.insertCell(0).innerHTML = cat.cat;
-        row.insertCell(1).innerHTML = "<a>modify</a>";
-        row.insertCell(2).innerHTML = "<a>delete</a>";
+        row.insertCell(1).innerHTML = "<a>Delete</a>";
     }
 }
+
+/*FINALDE AGREGAR CATEGORÍAS*/
+
+/*INICIO DE RESUMENES */
+function registerNewResume() {
+    var reg_title = document.getElementById("title").value;
+    var reg_sinop = document.getElementById("sinop").value;
+    var reg_recom = document.getElementById("recom").value;
+    var reg_infAutor = document.getElementById("infAutor").value;
+    var reg_contenido = document.getElementById("cont").value;
+
+
+    //alert(reg_user);
+    var resumeArray = [];
+
+    if (localStorage.getItem("ResumeAdminArray") !== null) {
+        resumeArray = JSON.parse(localStorage.getItem("ResumeAdminArray"));
+    }
+
+    var current_reg = {
+        //agregar registro de usuario
+        titulo: reg_title,
+        sinopsis: reg_sinop,
+        recomendacion: reg_recom,
+        infoAutor: reg_infAutor,
+        contenido: reg_contenido
+    };
+
+    resumeArray.push(current_reg);
+
+    localStorage.setItem("ResumeAdminArray", JSON.stringify(resumeArray));
+
+    //Limpiar valores de inputs
+    document.getElementById("title").value = "";
+    document.getElementById("sinop").value = "";
+    document.getElementById("recom").value = "";
+    document.getElementById("infAutor").value = "";
+    document.getElementById("cont").value = "";
+
+    loadResumeDataFromAdmin()
+
+    //window.location.href = "file:///C:/U/Documentos/Ciclo-01-2021/Progra%20II/ProyectoFinalParte/proyectoCat/index.html"
+    //window.location.href = "http://heroku:5000/login";
+}
+
+function loadResumeDataFromAdmin() {
+    var resumeArray
+    if (localStorage.getItem("ResumeAdminArray") !== null) {
+        resumeArray = JSON.parse(localStorage.getItem("ResumeAdminArray"));
+    }
+
+    var resumeTable = document.getElementById("resumeTable")
+    var row
+
+    for (var resume of resumeArray) {
+        row = resumeTable.insertRow(1);
+
+        //Insertar columna de usuario
+        row.insertCell(0).innerHTML = resume.titulo;
+        row.insertCell(1).innerHTML = resume.sinopsis;
+        row.insertCell(2).innerHTML = resume.recomendacion;
+        row.insertCell(3).innerHTML = resume.infoAutor;
+        row.insertCell(4).innerHTML = resume.contenido;
+        row.insertCell(5).innerHTML = "<a>Delete</a>";
+    }
+}
+
+
+/*FIN DE RESUMENES */
